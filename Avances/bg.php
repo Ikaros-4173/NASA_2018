0400f301be22a49ec0690aa1572c05f8c3298a1c
Ikaros-4173 <alexanderecheverria84@gmail.com>
2018-10-20 20:40:28 -0500

    Se presentan los primeros avances de nuestro proyecto

---
+++ Avances/bg.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<style>/*Se setea los botones de ingreso*/
+body
+{
+    background-image: url("fondo.jpg");
+    background-attachment: fixed; /* El fondo permanecerá fijo */
+    background-repeat: no-repeat;
+}
+p
+{
+background-color: black;
+color: white;
+opacity: 0.3;
+}
+</style>
+</body>
+</html>
---
+++ Avances/inicio.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>¡No olvides el abridor de latas!</title>
+  <?php require 'cabecera.php' ?>
+  <?php require 'bg.php' ?>
+  </head>
+  <body>
+
+    <h1>El caballo ira aqui</h1>
+      <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+      <link rel="stylesheet" href="assets/css/style.css">
+</body>
+</html>
